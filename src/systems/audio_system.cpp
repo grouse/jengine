a6bb6659a76424e47721985df6c25d36c880e6bc
Jesper Stefansson <steffe.jst@gmail.com>
2014-06-11 00:06:01 +0200

    basics for upcoming audio system

---
+++ src/systems/audio_system.cpp
@@ -0,0 +1,15 @@
+#include "audio_system.h"
+
+namespace JEngine {
+	namespace Systems {
+		AudioSystem::AudioSystem(Core::GameObjects* objects) : Core::System(objects) {
+
+		}
+
+		AudioSystem::~AudioSystem() {}
+
+		void AudioSystem::init() {}
+		void AudioSystem::update(float dt) {}	
+
+	}
+}
---
+++ src/systems/audio_system.h
@@ -0,0 +1,21 @@
+#ifndef AUDIO_SYSTEM_H
+#define AUDIO_SYSTEM_H
+
+#include "core/system.h"
+#include "core/game_objects.h"
+
+namespace JEngine {
+	namespace Systems {
+		class AudioSystem : public Core::System {
+			public:
+				AudioSystem(Core::GameObjects*);
+				virtual ~AudioSystem();
+
+				virtual void init();
+				virtual void update(float);	
+
+		};
+	}
+}
+
+#endif
